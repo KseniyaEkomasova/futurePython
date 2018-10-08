08400db6b1eb2478c266dc3abd8746c8e88d9fc3
KseniyaEkomasova <i231294@yandex.ru>
2018-10-08 12:35:37 +0300

    Добавила задачи 2 норм и задания 3его урока изи.

--- 2 HW es.py
+++ 2 HW es.py
@@ -152,3 +152,65 @@ for el in set(spi):
         lstThree.append(el)
 print("Ну а тут просто уникальный: ", lstThree)
 print("Мы молодцы!!!!")
+
+
+"""
+
+Задания урока 3
+Сделала только изи, остальное не успеваю, sorry!!!!!!!!!!!!
+
+
+"""
+
+# Задание-1:
+# Напишите функцию, округляющую полученное произвольное десятичное число
+# до кол-ва знаков (кол-во знаков передается вторым аргументом).
+# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
+# Для решения задачи не используйте встроенные функции и функции из модуля math.
+
+def my_round(number, ndigits):
+    number = number * (10 ** ndigits)
+    if float(number) - int(number) > 0.5:
+        number = number // 1 + 1
+    else:
+        number = number // 1
+    return number / (10 ** ndigits)
+
+
+
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
+
+
+# Задание-2:
+# Дан шестизначный номер билета. Определить, является ли билет счастливым.
+# Решение реализовать в виде функции.
+# Билет считается счастливым, если сумма его первых и последних цифр равны.
+# !!!P.S.: функция не должна НИЧЕГО print'ить
+
+def lucky_ticket(ticket_number):
+    ticket_len = len(str(ticket_number))
+    if ticket_len != 6:
+        return "Это неправильный билетик. Его определить не сможем :("
+    # теперь мы делаем строки, чтобы посчитать билет
+    str_ticket_number = str(ticket_number)
+    #разделим на правую и левую стороны
+    left_number = 0
+    right_number = 0
+    for i in range(3):
+        left_number += int(str_ticket_number[i])
+        right_number += int(str_ticket_number[i + 3])
+
+    if left_number == right_number:
+        return "Счастлвчик"
+
+    else:
+        return "Не счастливчик"
+
+
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
+
+
